9d3fc4cdd594e0f1d1d3958526c2e327fdde05a0
tjdakf <ksc73450056@gmail.com>
2025-11-08 03:06:17 +0900

    feat: implement device search function
    - SDK초기화를 위해 nvInit 실행 추가
    - 3초 동안 LAN에서 디바이스 검색

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,44 @@
 import com.sun.jna.Native;
 
 public class Application {
+    static Boolean g_bAPIReturn = false;
+    static int g_code = 0;
+    static String g_sn = "BZSA79353N1310006847";
 
-    public static void main(String[] args) {
+    static void waitAPIReturn() throws InterruptedException {
+        while (!g_bAPIReturn) {
+            Thread.sleep(1000);
+        }
+        g_bAPIReturn = false;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String rootDir = System.getProperty("user.dir").replaceAll("\\\\", "/");
+        String jnaDir = rootDir + "/bin";
+        String tempDir = rootDir + "/temp";
+        String companyInfo = "{\"company\":\"tjdakf\",\"phone\":\"010-1234-5678\",\"email\":\"ksc73450056@gmail.com\"}";
+
         System.setProperty("jna.encoding", "UTF-8");
-        System.setProperty("jna.library.path", System.getProperty("user.dir") + "\\bin");
+        System.setProperty("jna.library.path", jnaDir);
         ViplexCore viplexCore = (ViplexCore) Native.loadLibrary("viplexcore",ViplexCore.class);
+        ViplexCore.CallBack callBack = new ViplexCore.CallBack() {
+            @Override
+            public void dataCallBack(int code, String data) {
+                g_code = code;
+                String strCode = "\nViplexCore Demo code:" + code;
+                String strData = "\nViplexCore Demo data:" + data;
+                System.out.println(strCode);
+                System.out.println(strData);
+                g_bAPIReturn=true;
+            }
+        };
+
+        viplexCore.nvSetDevLang("Java");
+        System.out.print("nvInit(SDK초기화):");
+        System.out.println(viplexCore.nvInit(tempDir,companyInfo));
+        System.out.println("ViplexCore Demo nvSearchTerminalAsync(搜索) begin... ");
+        viplexCore.nvSearchTerminalAsync(callBack);
+        Thread.sleep(3000);
+        g_bAPIReturn = false;
     }
 }

de38e7949f771d6808dee7046f12d86aeb123432
tjdakf <ksc73450056@gmail.com>
2025-11-10 07:33:29 +0900

    feat: implement login/logout function
    - loadTemplate() 메서드로 JSON 요청 템플릿 로드 기능 구현
    - org.json 라이브러리 추가

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,17 @@
 import com.sun.jna.Native;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Application {
     static Boolean g_bAPIReturn = false;
-    static int g_code = 0;
-    static String g_sn = "BZSA79353N1310006847";
+    static int g_code;
+    static String g_data;
+    static String rootDir = System.getProperty("user.dir").replaceAll("\\\\", "/");
+    static String jnaDir = rootDir + "/bin";
+    static String tempDir = rootDir + "/temp";
 
     static void waitAPIReturn() throws InterruptedException {
         while (!g_bAPIReturn) {
@@ -12,19 +20,24 @@ public class Application {
         g_bAPIReturn = false;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        String rootDir = System.getProperty("user.dir").replaceAll("\\\\", "/");
-        String jnaDir = rootDir + "/bin";
-        String tempDir = rootDir + "/temp";
-        String companyInfo = "{\"company\":\"tjdakf\",\"phone\":\"010-1234-5678\",\"email\":\"ksc73450056@gmail.com\"}";
+    public static String loadTemplate(String name) {
+        try {
+            return Files.readString(Paths.get(rootDir, "resources", name));
+        } catch (IOException e) {
+            throw new RuntimeException("템플릿 로드 실패: " + name, e);
+        }
+    }
 
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("jna.encoding", "UTF-8");
         System.setProperty("jna.library.path", jnaDir);
         ViplexCore viplexCore = (ViplexCore) Native.loadLibrary("viplexcore",ViplexCore.class);
+
         ViplexCore.CallBack callBack = new ViplexCore.CallBack() {
             @Override
             public void dataCallBack(int code, String data) {
                 g_code = code;
+                g_data = data;
                 String strCode = "\nViplexCore Demo code:" + code;
                 String strData = "\nViplexCore Demo data:" + data;
                 System.out.println(strCode);
@@ -34,11 +47,39 @@ public class Application {
         };
 
         viplexCore.nvSetDevLang("Java");
+        String companyInfo = loadTemplate("init-request.json");
         System.out.print("nvInit(SDK초기화):");
         System.out.println(viplexCore.nvInit(tempDir,companyInfo));
-        System.out.println("ViplexCore Demo nvSearchTerminalAsync(搜索) begin... ");
+
+        System.out.println("=== 장치 검색 시작 ===");
         viplexCore.nvSearchTerminalAsync(callBack);
         Thread.sleep(3000);
         g_bAPIReturn = false;
+
+        if (g_data == null) {
+            System.out.println("=== 장치 검색 실패 ===");
+            return;
+        }
+
+        JSONObject obj = new JSONObject(g_data);
+        String sn = obj.getString("sn");
+        obj = new JSONObject(loadTemplate("login-request.json"));
+        obj.put("sn", sn);
+        System.out.println("=== 장치에 로그인 시작 ===");
+        viplexCore.nvLoginAsync(obj.toString(), callBack);
+        waitAPIReturn();
+        if (g_code != 0) {
+            System.out.println("=== 로그인 실패 ===");
+            return;
+        }
+
+        Thread.sleep(3000);
+
+        obj = new JSONObject(loadTemplate("logout-request.json"));
+        obj.put("sn", sn);
+        System.out.println("=== 장치 로그아웃 시작 ===");
+        viplexCore.nvLogoutAsync(obj.toString(), callBack);
+        waitAPIReturn();
+        System.out.println("=== 로그아웃 완료 ===");
     }
 }

022fcf818852d365c84cc1fa30973470645af340
tjdakf <ksc73450056@gmail.com>
2025-11-14 17:18:30 +0900

    feat: 터미널 정보 저장 기능 추가
    - 이름, SN, 해상도, 로그인/비밀번호 여부 저장

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,6 +1,19 @@
 import org.json.JSONObject;
 
 public class Terminal {
+    private String aliasName;
+    private String sn;
+    private int width;
+    private int height;
+    private boolean logined;
+    private boolean hasPassword;
+
     public Terminal(JSONObject data) {
+        this.aliasName = data.getString("aliasName");
+        this.sn = data.getString("sn");
+        this.width = data.getInt("width");
+        this.height = data.getInt("height");
+        this.logined = data.getBoolean("logined");
+        this.hasPassword = data.getBoolean("hasPassword");
     }
 }

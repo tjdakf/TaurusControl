983a01debd1361bed8069e347055a20fdda6049a
tjdakf <ksc73450056@gmail.com>
2025-11-21 09:19:44 +0900

    refactor: AsyncHelper에 volatile 추가로 thread-safety 보장
    - apiReturn 변수에 volatile 키워드 추가
    - 멀티스레드 환경에서 메모리 가시성 보장
    - SDK 콜백과 Service 스레드 간 안전한 통신 확보

--- src/main/java/tauruscontrol/util/AsyncHelper.java
+++ src/main/java/tauruscontrol/util/AsyncHelper.java
@@ -3,7 +3,9 @@ package tauruscontrol.util;
 public class AsyncHelper {
     private static final int WAIT_INTERVAL = 1000;
 
-    static boolean apiReturn = false;
+    // volatile: 멀티스레드 환경에서 메모리 가시성 보장
+    // SDK 콜백(다른 스레드)에서 설정한 값을 waitAPIReturn()에서 즉시 확인 가능
+    private static volatile boolean apiReturn = false;
 
     public static void waitAPIReturn() {
         try {
